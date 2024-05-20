cff6b552e8a74d12c22272d3a3658dfcbc60142f
Alexander Nelson Leano <31350429+LeanoA@users.noreply.github.com>
2024-05-20 13:43:57 -0300

    adding ruff implementation

--- iolpy/stocks.py
+++ iolpy/stocks.py
@@ -2,7 +2,6 @@ from .util.const import _ROOT_URL_
 import requests
 
 from bs4 import BeautifulSoup
-import requests
 import pandas as pd
 
 
@@ -12,36 +11,35 @@ def get_stocks():
     # print("Response: ", response.status_code)
     # print(response.text[:200])
 
-    soup = BeautifulSoup(response.text, 'html.parser')
+    soup = BeautifulSoup(response.text, "html.parser")
     # print(soup.title)
     # print(soup.prettify())
 
-    html_tables = soup.find_all('table')
+    html_tables = soup.find_all("table")
     # print("Cantidad de tablas: ", len(html_tables))
 
     first_table = html_tables[0]
 
     # Apply find_all() function with `tr` element on first_table
-    all_tr = first_table.find('tbody').find_all('tr')
+    all_tr = first_table.find("tbody").find_all("tr")
     # print(all_tr[0].prettify())
 
     cotization_table = []
 
     for row in all_tr:
         row_data = {}
-        columns = row.find_all('td')
+        columns = row.find_all("td")
         for column in columns:
             try:
                 # data-field is the name of the field
                 # data-order is the value of the field
-                row_data[column['data-field']] = column['data-order']
-            except:
+                row_data[column["data-field"]] = column["data-order"]
+            except Exception as e:
                 try:
-                    row_data['Simbolo'] = ''.join(
-                        [x for x in column.find('b').text if x not in [' ', '\r', '\n']])
-                    row_data['Nombre'] = column.find('i')['title']
-                except:
-                    pass
+                    row_data["Simbolo"] = "".join([x for x in column.find("b").text if x not in [" ", "\r", "\n"]])
+                    row_data["Nombre"] = column.find("i")["title"]
+                except Exception as e:
+                    print(f"How exceptional! {e}")
         cotization_table.append(row_data)
 
     cotization_df = pd.DataFrame(cotization_table)
